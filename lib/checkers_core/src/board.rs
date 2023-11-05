a54339b94f505573d20b378cb172fde9616391c9
0xRuFFy <konstantinopora@gmail.com>
2023-11-05 22:17:36 +0100

    finished get possible_moves and clean up

--- lib/checkers_core/src/board.rs
+++ lib/checkers_core/src/board.rs
@@ -1,20 +1,25 @@
 use std::fmt::{Display, Formatter, Result};
+use colored::*;
 
 pub type PieceColor = bool;
 pub const WHITE: PieceColor = true;
 pub const BLACK: PieceColor = false;
 
 pub type Bitboard = u64;
-// const DEFAULT_WHITE: Bitboard = 0x000000000055aa55;
-// const DEFAULT_BLACK: Bitboard = 0xaa55aa0000000000;
-pub const DEFAULT_WHITE: Bitboard = 0x0000000000550000;
-pub const DEFAULT_BLACK: Bitboard = 0x000000000000aa00;
+const DEFAULT_WHITE: Bitboard = 0x000000000055aa55;
+const DEFAULT_BLACK: Bitboard = 0xaa55aa0000000000;
+// pub const DEFAULT_WHITE: Bitboard = 0x0000000000550010;
+// pub const DEFAULT_BLACK: Bitboard = 0x000000000000aa00;
 
 pub type PieceType = bool;
 pub const MAN: PieceType = false;
 pub const KING: PieceType = true;
 
 type Piece = (PieceColor, PieceType);
+
+pub type MoveType = bool;
+pub const MOVE: MoveType = false;
+pub const JUMP: MoveType = true;
 
 pub struct Board {
     white: Bitboard,
@@ -24,15 +29,15 @@ pub struct Board {
     kings: Bitboard,
 }
 
-// Note: Most of the following methods [pub(crate)] are not part of the public API. --> the validity of the arguments is not checked. !!!
+// Note: Most of the following methods are not checked for validity of their arguments. !!!
 impl Board {
     // ------------------- CONSTRUCTOR -------------------
     pub fn new() -> Board {
         return Board {
             white: DEFAULT_WHITE,
             black: DEFAULT_BLACK,
-            kings: 0x0000000000550000,
-            // kings: 0,
+            // kings: 0x0000000000550000,
+            kings: 0,
         };
     }
 
@@ -50,16 +55,8 @@ impl Board {
     }
 
     // ------------------- PRIVATE -------------------
-    fn is_valid_and_empty(&self, id: u8) -> bool {
-        return Self::is_valid_id(id) && (self.white | self.black) & 1 << id == 0;
-    }
-
     fn is_valid_and_empty_signed(&self, id: i8) -> bool {
         return Self::is_valid_id_signed(id) && (self.white | self.black) & 1 << id as u8 == 0;
-    }
-
-    fn is_valid_and_occupied(&self, id: u8, bitboard: Bitboard) -> bool {
-        return Self::is_valid_id(id) && bitboard & 1 << id != 0;
     }
 
     fn is_valid_and_occupied_signed(&self, id: i8, bitboard: Bitboard) -> bool {
@@ -122,10 +119,16 @@ impl Board {
         }
 
         let mut moves = Vec::new();
+        let mut can_jump = false;
 
         for i in 0..64 {
             if bitboard & 1 << i != 0 {
-                moves.push((i, self.get_possible_moves_of(i).unwrap()));
+                if let Some((moves_of_i, move_type)) = self.get_possible_moves_of(i) {
+                    can_jump |= move_type;
+                    if moves_of_i.len() > 0 && (!can_jump || move_type) {
+                        moves.push((i, moves_of_i));
+                    }
+                }
             }
             if moves.len() == count {
                 break;
@@ -154,52 +157,45 @@ impl Board {
         return self.get_piece(id);
     }
 
-    pub(crate) fn move_piece(&mut self, from: u8, to: u8) {
-        if self.white & 1 << from != 0 {
-            self.white &= !(1 << from);
-            self.white |= 1 << to;
-        } else if self.black & 1 << from != 0 {
-            self.black &= !(1 << from);
-            self.black |= 1 << to;
-        }
-
-        if self.kings & 1 << from != 0 {
-            self.kings &= !(1 << from);
-            self.kings |= 1 << to;
-        }
-    }
-
-    pub(crate) fn remove_piece(&mut self, id: u8) {
-        self.white &= !(1 << id);
-        self.black &= !(1 << id);
-        self.kings &= !(1 << id);
-    }
-
-    pub(crate) fn king_piece(&mut self, id: u8) {
-        self.kings |= 1 << id;
-    }
-
-    // TODO
-    // pub(crate) fn get_possible_moves_of(&self, id: u8) -> Option<Vec<u8>> {
-    pub fn get_possible_moves_of(&self, id: u8) -> Option<Vec<u8>> {
+    pub(crate) fn get_possible_moves_of(&self, id: u8) -> Option<(Vec<u8>, MoveType)> {
         let piece = self.get_piece(id)?;
         let (color, piece_type) = piece;
-        // let dir = if color == WHITE { 1 } else { -1 };
 
-        // let mut moves = Vec::new();
+        let jumps = self.get_possible_jumps_for(id, piece_type, color);
+        if jumps.len() > 0 {
+            return Some((jumps, JUMP));
+        }
 
-        // let mut jumps = self.get_possible_jumps_for(id, piece_type, color);
-
-
-        // return Some(self.get_open_squares_for(id, piece_type, color));
-        return Some(self.get_possible_jumps_for(id, piece_type, color));
-    }
-
-    pub(crate) fn get_possible_moves_of_by_coords(&self, row: u8, col: u8) -> Option<Vec<u8>> {
-        return self.get_possible_moves_of(Self::get_id_from_coords(row, col));
+        return Some((self.get_open_squares_for(id, piece_type, color), MOVE));
     }
 
     // ------------------- PUBLIC -------------------
+    pub fn to_string(&self, possible_moves: Vec<(u8, Vec<u8>)>) -> String {
+        let mut board = String::new();
+
+        for row in (0..8).rev() {
+            for col in 0..8 {
+                let highlight = possible_moves.iter().any(|x| x.0 == Self::get_id_from_coords(row, col));
+                board.push_str(
+                    format!(
+                        "|{}",
+                        match self.get_piece_by_coords(row, col) {
+                            Some((WHITE, MAN)) => 'w',
+                            Some((BLACK, MAN)) => 'b',
+                            Some((WHITE, KING)) => 'W',
+                            Some((BLACK, KING)) => 'B',
+                            None => ' ',
+                        }.to_string().color(if highlight { Color::Green } else { Color::White })
+                    )
+                    .as_str(),
+                );
+            }
+            board.push_str("|\n");
+        }
+
+        return board;
+    }
+    
     pub fn white_count(&self) -> u8 {
         return self.white.count_ones() as u8;
     }
@@ -223,6 +219,31 @@ impl Board {
 
     pub fn get_black_possible_moves(&self) -> Vec<(u8, Vec<u8>)> {
         return self.get_color_possible_moves(BLACK);
+    }
+
+    pub fn move_piece(&mut self, from: u8, to: u8) {
+        if self.white & 1 << from != 0 {
+            self.white &= !(1 << from);
+            self.white |= 1 << to;
+        } else if self.black & 1 << from != 0 {
+            self.black &= !(1 << from);
+            self.black |= 1 << to;
+        }
+
+        if self.kings & 1 << from != 0 {
+            self.kings &= !(1 << from);
+            self.kings |= 1 << to;
+        }
+    }
+
+    pub fn remove_piece(&mut self, id: u8) {
+        self.white &= !(1 << id);
+        self.black &= !(1 << id);
+        self.kings &= !(1 << id);
+    }
+
+    pub fn king_piece(&mut self, id: u8) {
+        self.kings |= 1 << id;
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,12 @@ use checkers_core as core;
 fn main() {
     println!("Hello, world!");
 
-    let mut board = core::Board::new();
+    let board = core::Board::new();
 
+    // println!("{}", board.to_string(board.get_white_possible_moves()));
     println!("{}", board);
 
-    println!("{:?}", board.get_possible_moves_of(16));
-    // for i in board.get_possible_moves_of(2).unwrap() {
-    //     println!("{}", core::Board::is_valid_id(i));
-    // }
-
     println!("{:?}", board.get_white_possible_moves());
-        
+
     // println!("{}", board);
 }
