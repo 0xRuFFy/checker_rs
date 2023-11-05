9d1e758c2fa96602344ce3e86a6a81a81d680214
0xRuFFy <konstantinopora@gmail.com>
2023-11-05 20:14:26 +0100

    started on the jump implementation

--- lib/checkers_core/src/board.rs
+++ lib/checkers_core/src/board.rs
@@ -1,18 +1,18 @@
 use std::fmt::{Display, Formatter, Result};
 
-type PieceColor = bool;
-const WHITE: PieceColor = true;
-const BLACK: PieceColor = false;
+pub type PieceColor = bool;
+pub const WHITE: PieceColor = true;
+pub const BLACK: PieceColor = false;
 
-type Bitboard = u64;
+pub type Bitboard = u64;
 // const DEFAULT_WHITE: Bitboard = 0x000000000055aa55;
 // const DEFAULT_BLACK: Bitboard = 0xaa55aa0000000000;
-const DEFAULT_WHITE: Bitboard = 0x0000000000000055;
-const DEFAULT_BLACK: Bitboard = 0x0000000000000000;
+pub const DEFAULT_WHITE: Bitboard = 0x0000000000550000;
+pub const DEFAULT_BLACK: Bitboard = 0x000000000000aa00;
 
-type PieceType = bool;
-const MAN: PieceType = false;
-const KING: PieceType = true;
+pub type PieceType = bool;
+pub const MAN: PieceType = false;
+pub const KING: PieceType = true;
 
 type Piece = (PieceColor, PieceType);
 
@@ -31,7 +31,7 @@ impl Board {
         return Board {
             white: DEFAULT_WHITE,
             black: DEFAULT_BLACK,
-            kings: 0x0000000000000055,
+            kings: 0x0000000000550000,
             // kings: 0,
         };
     }
@@ -41,23 +41,31 @@ impl Board {
         return row * 8 + col;
     }
 
-    pub fn is_valid_id(id: u8) -> bool {
+    pub(crate) fn is_valid_id(id: u8) -> bool {
         return id < 64 && (id % 8 + id / 8) % 2 == 0;
     }
 
-    pub fn is_valid_id_signed(id: i8) -> bool {
+    pub(crate) fn is_valid_id_signed(id: i8) -> bool {
         return id >= 0 && Self::is_valid_id(id as u8);
     }
 
-    pub fn is_valid_and_empty(&self, id: u8) -> bool {
+    // ------------------- PRIVATE -------------------
+    fn is_valid_and_empty(&self, id: u8) -> bool {
         return Self::is_valid_id(id) && (self.white | self.black) & 1 << id == 0;
     }
 
-    pub fn is_valid_and_empty_signed(&self, id: i8) -> bool {
+    fn is_valid_and_empty_signed(&self, id: i8) -> bool {
         return Self::is_valid_id_signed(id) && (self.white | self.black) & 1 << id as u8 == 0;
     }
 
-    // ------------------- PRIVATE -------------------
+    fn is_valid_and_occupied(&self, id: u8, bitboard: Bitboard) -> bool {
+        return Self::is_valid_id(id) && bitboard & 1 << id != 0;
+    }
+
+    fn is_valid_and_occupied_signed(&self, id: i8, bitboard: Bitboard) -> bool {
+        return Self::is_valid_id_signed(id) && bitboard & 1 << id as u8 != 0;
+    }
+
     fn count_kings(&self, color: PieceColor) -> u8 {
         return (if color == WHITE {
             self.white
@@ -65,6 +73,66 @@ impl Board {
             self.black
         } & self.kings)
             .count_ones() as u8;
+    }
+
+    // Note: this is compact but may be slow.
+    fn get_open_squares_for(&self, id: u8, piece_type: PieceType, color: PieceColor) -> Vec<u8> {
+        let dir = if color == WHITE { 1 } else { -1 };
+
+        let mut s = vec![7, 9];
+        if piece_type == KING {
+            s.append(&mut vec![-7, -9]);
+        }
+        return s
+            .into_iter()
+            .map(|x| id as i8 + x * dir)
+            .filter(|x| self.is_valid_and_empty_signed(*x))
+            .map(|x| x as u8)
+            .collect();
+    }
+
+    fn get_possible_jumps_for(&self, id: u8, piece_type: PieceType, color: PieceColor) -> Vec<u8> {
+        let dir = if color == WHITE { 1 } else { -1 };
+        let occ = if color == WHITE {
+            self.black
+        } else {
+            self.white
+        };
+
+        let mut s = vec![7, 9];
+        if piece_type == KING {
+            s.append(&mut vec![-7, -9]);
+        }
+        return s
+            .into_iter()
+            .map(|x| (id as i8 + x * dir, x * dir))
+            .filter(|x| self.is_valid_and_occupied_signed((*x).0, occ) && self.is_valid_and_empty_signed((*x).0 + (*x).1))
+            .map(|x| (x.0 + x.1) as u8)
+            .collect();
+    }
+
+    fn get_color_possible_moves(&self, color: PieceColor) -> Vec<(u8, Vec<u8>)> {
+        let (count, bitboard) = if color == WHITE {
+            (self.white_count() as usize, self.white)
+        } else {
+            (self.black_count() as usize, self.black)
+        };
+        if count == 0 {
+            return Vec::new();
+        }
+
+        let mut moves = Vec::new();
+
+        for i in 0..64 {
+            if bitboard & 1 << i != 0 {
+                moves.push((i, self.get_possible_moves_of(i).unwrap()));
+            }
+            if moves.len() == count {
+                break;
+            }
+        }
+
+        return moves;
     }
 
     // ------------------- PUB(CRATE) -------------------
@@ -116,37 +184,15 @@ impl Board {
     pub fn get_possible_moves_of(&self, id: u8) -> Option<Vec<u8>> {
         let piece = self.get_piece(id)?;
         let (color, piece_type) = piece;
-        let dir = if color == WHITE { 1 } else { -1 };
+        // let dir = if color == WHITE { 1 } else { -1 };
 
-        let mut possible_moves = Vec::new();
+        // let mut moves = Vec::new();
 
-        let m = id as i8 + 7 * dir;
-        if self.is_valid_and_empty_signed(m) {
-            possible_moves.push(m as u8);
-        }
-
-        let m = id as i8 + 9 * dir;
-        if self.is_valid_and_empty_signed(m) {
-            possible_moves.push(m as u8);
-        }
-
-        if piece_type == KING {
-            let m = id as i8 - 7 * dir;
-            if self.is_valid_and_empty_signed(m) {
-                possible_moves.push(m as u8);
-            }
-
-            let m = id as i8 - 9 * dir;
-            if self.is_valid_and_empty_signed(m) {
-                possible_moves.push(m as u8);
-            }
-        }
+        // let mut jumps = self.get_possible_jumps_for(id, piece_type, color);
 
 
-        // possible_moves.push((id as i8 + 7 * dir) as u8);
-        // possible_moves.push((id as i8 + 9 * dir) as u8);
-
-        return Some(possible_moves);
+        // return Some(self.get_open_squares_for(id, piece_type, color));
+        return Some(self.get_possible_jumps_for(id, piece_type, color));
     }
 
     pub(crate) fn get_possible_moves_of_by_coords(&self, row: u8, col: u8) -> Option<Vec<u8>> {
@@ -172,11 +218,11 @@ impl Board {
 
     // TODO: Could use a hashmap to store the possible moves of each piece. (more memory, less computation is expected but needs to be tested in comparison to Vector lookup)
     pub fn get_white_possible_moves(&self) -> Vec<(u8, Vec<u8>)> {
-        todo!();
+        return self.get_color_possible_moves(WHITE);
     }
 
     pub fn get_black_possible_moves(&self) -> Vec<(u8, Vec<u8>)> {
-        todo!();
+        return self.get_color_possible_moves(BLACK);
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
-use checkers_core::Board;
+use checkers_core as core;
 
 fn main() {
     println!("Hello, world!");
 
-    let mut board = Board::new();
+    let mut board = core::Board::new();
 
     println!("{}", board);
 
-    println!("{:?}", board.get_possible_moves_of(2));
-    for i in board.get_possible_moves_of(2).unwrap() {
-        println!("{}", Board::is_valid_id(i));
-    }
+    println!("{:?}", board.get_possible_moves_of(16));
+    // for i in board.get_possible_moves_of(2).unwrap() {
+    //     println!("{}", core::Board::is_valid_id(i));
+    // }
 
+    println!("{:?}", board.get_white_possible_moves());
+        
     // println!("{}", board);
 }
