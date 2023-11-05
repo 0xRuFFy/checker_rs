e96de3967136204dc62f14b13fbfe928f6c2380c
0xRuFFy <konstantinopora@gmail.com>
2023-11-05 15:12:10 +0100

    test board initialization and move piece functionality

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+#![allow(dead_code)]
+
 mod core;
 
 use core::Board;
 
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
 
-    let board = Board::new();
+    let mut board = Board::new();
+
+    println!("{}", board);
+    
+    board.move_piece(16, 25);
 
     println!("{}", board);
 }
