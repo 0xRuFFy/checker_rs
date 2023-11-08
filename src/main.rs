d71e40f621578027e715ce8e7a4d44630c07440a
0XRuFFy <66907424+0xRuFFy@users.noreply.github.com>
2023-11-08 12:25:16 +0100

    playing around

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,9 @@ fn main() {
 
     let mut game = CheckersGame::new(
         Box::new(player::HumanPlayer::new()),
-        Box::new(player::BotPlayer::minimax(bot_player::Depth::Dynamic, 2)),
-        // Box::new(player::BotPlayer::minimax(bot_player::Depth::Static(6), 2)),
+        // Box::new(player::BotPlayer::minimax(bot_player::Depth::Dynamic, 2)),
+        // Box::new(player::BotPlayer::alpha_beta(bot_player::Depth::Static(6), 2)),
+        Box::new(player::BotPlayer::minimax(bot_player::Depth::Static(6), 2)),
         // Box::new(player::BotPlayer::minimax(bot_player::Depth::Static(5), 1)),
         // Box::new(player::HumanPlayer::new()),
         // Box::new(player::BotPlayer::minimax(bot_player::Depth::Dynamic, 2)),
